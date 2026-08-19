6b25ea9a7da802baf6477cf9ac625d62b6b6effa
jahdaganj00ki-eng <jahdaganj00ki@gmail.com>
2026-08-19 19:00:39 +0200

    Add files via upload

--- windows-app/build.gradle.kts
+++ windows-app/build.gradle.kts
@@ -13,6 +13,7 @@ repositories {
 
 dependencies {
     implementation(compose.desktop.currentOs)
+    implementation(compose.material3)
     implementation(compose.materialIconsExtended)
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.json:json:20240303")
