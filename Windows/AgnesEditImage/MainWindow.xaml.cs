561785db81f361b330cb575ad5feb9aee26e991b
jahdaganj00ki-eng <jahdaganj00ki@gmail.com>
2026-09-16 04:48:57 +0000

    fix: add missing usings for Button and ResultImage in MainWindow.xaml.cs

--- Windows/AgnesEditImage/MainWindow.xaml.cs
+++ Windows/AgnesEditImage/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media.Imaging;
 using AgnesEditImage.ViewModels;
+using AgnesEditImage.Models;
 using AgnesEditImage.Services;
 
 namespace AgnesEditImage;
