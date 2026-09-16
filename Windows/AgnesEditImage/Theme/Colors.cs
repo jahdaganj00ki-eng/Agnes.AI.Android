a8047b1b8d25c8e57d3a11c3330220cfc23475b6
jahdaganj00ki-eng <jahdaganj00ki@gmail.com>
2026-09-16 02:48:57 +0000

    fix: ThemeColors Brush properties for WPF Background/Foreground
    XAML Background/Foreground require Brush, not Color. Add Brush
    properties and keep Color aliases for any direct color usage.

--- Windows/AgnesEditImage/Theme/Colors.cs
+++ Windows/AgnesEditImage/Theme/Colors.cs
@@ -4,13 +4,23 @@ namespace AgnesEditImage.Theme;
 
 public static class ThemeColors
 {
-    public static readonly Color AppBackground = Color.FromRgb(0x12, 0x12, 0x12);
-    public static readonly Color CardBackground = Color.FromRgb(0x1E, 0x1E, 0x1E);
-    public static readonly Color CardBackground2 = Color.FromRgb(0x2A, 0x2A, 0x2E);
-    public static readonly Color UserBubble = Color.FromRgb(0x81, 0x84, 0xFD);
-    public static readonly Color TealBadge = Color.FromRgb(0x2D, 0xD4, 0xBF);
-    public static readonly Color TextPrimary = Color.FromRgb(0xF5, 0xF5, 0xF5);
-    public static readonly Color TextMuted = Color.FromRgb(0x9E, 0x9E, 0x9E);
-    public static readonly Color SparkleCyan = Color.FromRgb(0x8A, 0xD8, 0xF0);
-    public static readonly Color ErrorRed = Color.FromRgb(0xEF, 0x6A, 0x6A);
+    public static readonly Color AppBackgroundColor = Color.FromRgb(0x12, 0x12, 0x12);
+    public static readonly Color CardBackgroundColor = Color.FromRgb(0x1E, 0x1E, 0x1E);
+    public static readonly Color CardBackground2Color = Color.FromRgb(0x2A, 0x2A, 0x2E);
+    public static readonly Color UserBubbleColor = Color.FromRgb(0x81, 0x84, 0xFD);
+    public static readonly Color TealBadgeColor = Color.FromRgb(0x2D, 0xD4, 0xBF);
+    public static readonly Color TextPrimaryColor = Color.FromRgb(0xF5, 0xF5, 0xF5);
+    public static readonly Color TextMutedColor = Color.FromRgb(0x9E, 0x9E, 0x9E);
+    public static readonly Color SparkleCyanColor = Color.FromRgb(0x8A, 0xD8, 0xF0);
+    public static readonly Color ErrorRedColor = Color.FromRgb(0xEF, 0x6A, 0x6A);
+
+    public static readonly Brush AppBackground = new SolidColorBrush(AppBackgroundColor);
+    public static readonly Brush CardBackground = new SolidColorBrush(CardBackgroundColor);
+    public static readonly Brush CardBackground2 = new SolidColorBrush(CardBackground2Color);
+    public static readonly Brush UserBubble = new SolidColorBrush(UserBubbleColor);
+    public static readonly Brush TealBadge = new SolidColorBrush(TealBadgeColor);
+    public static readonly Brush TextPrimary = new SolidColorBrush(TextPrimaryColor);
+    public static readonly Brush TextMuted = new SolidColorBrush(TextMutedColor);
+    public static readonly Brush SparkleCyan = new SolidColorBrush(SparkleCyanColor);
+    public static readonly Brush ErrorRed = new SolidColorBrush(ErrorRedColor);
 }
